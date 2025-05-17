ea240f00ed527e56b89c761ebc2364cc55f666cb
Lucilêda <luciledaneta05@gmail.com>
2025-05-17 10:22:43 -0300

    codigo em c

---
+++ c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int entrada;
+    scanf("%d", &entrada);
+    if (entrada < 1 && entrada > 3)
+    {
+        printf("Opção inválida\n");
+        return 0;
+    }
+
+    else
+    {
+        
+    }
+}
